f477773e1cf57a4356908fc3dcd48f944e78e3c2
John Nguyen <john12052003@gmail.com>
2024-07-09 13:14:48 +0700

    Fix everything

--- front-end/src/assets/data/UnitChapter/index.js
+++ front-end/src/assets/data/UnitChapter/index.js
@@ -1,6 +1,60 @@
-import UnitChapter1 from './UnitChapter1';
-import UnitChapter2 from './UnitChapter2';
-import UnitChapter3 from './UnitChapter3';
-import UnitChapter4 from './UnitChapter4';
+import * as TrainingMaterial from '../TrainingMaterial';
 
-export { UnitChapter1, UnitChapter2, UnitChapter3, UnitChapter4 };
+export const UnitChapter = [{
+    id: 1,
+    name: 'MVC architectural pattern overview',
+    standard: 'K6SD',
+    duration: 10,
+    status: 'Online',
+    delivery_type_id: 2,
+    training_materials: [
+        TrainingMaterial.TrainingMaterial1,
+        TrainingMaterial.TrainingMaterial2,
+        TrainingMaterial.TrainingMaterial3,
+        TrainingMaterial.TrainingMaterial4,
+    ],
+},
+{
+    id: 2,
+    name: 'ASP.NET MVC Version History',
+    standard: 'K6SD',
+    duration: 10,
+    status: 'Offline',
+    delivery_type_id: 2,
+    training_materials: [
+        TrainingMaterial.TrainingMaterial1,
+        TrainingMaterial.TrainingMaterial2,
+        TrainingMaterial.TrainingMaterial3,
+        TrainingMaterial.TrainingMaterial4,
+    ],
+},
+{
+    id: 3,
+    name: 'ASP.NET MVC Folder Structure',
+    standard: 'K6SD',
+    duration: 30,
+    status: 'Offline',
+    delivery_type_id: 3,
+    training_materials: [
+        TrainingMaterial.TrainingMaterial1,
+        TrainingMaterial.TrainingMaterial2,
+        TrainingMaterial.TrainingMaterial3,
+        TrainingMaterial.TrainingMaterial4,
+    ],
+},
+{
+    id: 4,
+    name: 'Controllers in ASP.NET MVC Application',
+    standard: 'K6SD',
+    duration: 30,
+    status: 'Offline',
+    delivery_type_id: 6,
+    training_materials: [
+        TrainingMaterial.TrainingMaterial1,
+        TrainingMaterial.TrainingMaterial2,
+        TrainingMaterial.TrainingMaterial3,
+        TrainingMaterial.TrainingMaterial4,
+    ],
+}
+]
+
