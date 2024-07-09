083a13f885e5927860ec00e1b6ee68c0bf6ec1ed
John Nguyen <john12052003@gmail.com>
2024-07-09 13:09:06 +0700

    Fix everything

--- front-end/src/assets/data/Syllabus/index.js
+++ front-end/src/assets/data/Syllabus/index.js
@@ -1,8 +1,114 @@
-import Syllabus1 from './Syllabus1';
-import Syllabus2 from './Syllabus2';
-import Syllabus3 from './Syllabus3';
-import Syllabus4 from './Syllabus4';
-import Syllabus5 from './Syllabus5';
-import Syllabus6 from './Syllabus6';
+import moment from 'moment/moment';
 
-export { Syllabus1, Syllabus2, Syllabus3, Syllabus4, Syllabus5, Syllabus6 };
+import * as SyllabusDay from '../SyllabusDay';
+
+export const Syllabus = [{
+    id: 1,
+    code: 'LIN',
+    version: '2.0',
+    name: 'Linux',
+    status: 'Active',
+    days: 4,
+    hours: 12,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Johny Deep',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+},
+
+{
+    id: 2,
+    code: 'AWB',
+    version: '1.0',
+    name: 'AWS basic',
+    status: 'Active',
+    days: 7,
+    hours: 21,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Warrior Tran',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+},
+{
+    id: 3,
+    code: 'DOC',
+    version: '1.5',
+    name: 'Docker',
+    status: 'Active',
+    days: 3,
+    hours: 12,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Warrior Tran',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+},
+{
+    id: 4,
+    code: 'KUB',
+    version: '1.5',
+    name: 'Kubernetes',
+    status: 'Active',
+    days: 6,
+    hours: 18,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Ba Chu Heo',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+},
+{
+    id: 5,
+    code: 'DEC',
+    version: '2',
+    name: 'DevOps_CICD',
+    status: 'Active',
+    days: 8,
+    hours: 24,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Ba Chu Heo',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+},
+{
+    id: 6,
+    code: 'MOC',
+    version: '2.5',
+    name: 'Mock Project',
+    status: 'Inactive',
+    days: 3,
+    hours: 12,
+    modified_on: moment('2022-07-23').format('DD/MM/YYYY'),
+    modified_by: 'Ba Chu Heo',
+    syllabus_days: [
+        SyllabusDay.SyllabusDay1,
+        SyllabusDay.SyllabusDay2,
+        SyllabusDay.SyllabusDay3,
+        SyllabusDay.SyllabusDay4,
+        SyllabusDay.SyllabusDay5,
+    ],
+}
+]
