4a83c84e1f22abae5db304b77ef668c07c929f26
John Nguyen <john12052003@gmail.com>
2024-07-09 13:11:43 +0700

    Fix everything

--- front-end/src/assets/data/SyllabusUnit/index.js
+++ front-end/src/assets/data/SyllabusUnit/index.js
@@ -1,4 +1,27 @@
-import SyllabusUnit1 from './SyllabusUnit1';
-import SyllabusUnit2 from './SyllabusUnit2';
+import * as UnitChapter from '../UnitChapter';
+export const SyllabusUnit = [{
+    id: 1,
+    unit_no: 6,
+    name: 'MVC architecture in ASP.NET',
+    duration: 6,
+    unit_chapters: [
+        UnitChapter.UnitChapter1,
+        UnitChapter.UnitChapter2,
+        UnitChapter.UnitChapter3,
+        UnitChapter.UnitChapter4,
+    ],
+},
 
-export { SyllabusUnit1, SyllabusUnit2 };
+{
+    id: 2,
+    unit_no: 7,
+    name: 'Routing in MVC',
+    duration: 5.5,
+    unit_chapters: [
+        UnitChapter.UnitChapter1,
+        UnitChapter.UnitChapter2,
+        UnitChapter.UnitChapter3,
+        UnitChapter.UnitChapter4,
+    ],
+}
+]
