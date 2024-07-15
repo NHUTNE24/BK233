d01d698912e5841f047d417710c813f96a1af612
ThanhAnh <dangthanhanh186@gmail.com>
2024-07-15 23:17:14 +0700

    Update api gateway router

---
+++ apiGateway/api/src/test/java/com/fams/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fams.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
