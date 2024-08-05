c6a2aec044d5fb95259cc4b9f54742bd0c2ee47b
Tan Truong <tanfctk@gmail.com>
2024-08-05 14:25:22 +0700

    fix layout + add .env

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,32 +1,40 @@
-import imageLogo from "src/assets/images/logo.png";
-import imageUserLogo from "src/assets/images/cat.png";
-import imagefpt from "src/assets/images/logo1.png";
+import imageLogo from 'src/assets/images/logo.png';
+import imageUserLogo from 'src/assets/images/cat.png';
+import imagefpt from 'src/assets/images/logo1.png';
 import LogoutButton from '../../components/LogOut';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
-	const username = useSelector((state) => state.auth?.username || '');
-	const avatar = useSelector((state) => state.auth?.avatar || imageUserLogo);
-	return (
-		<header className="sticky text-primary border-b-2 top-0 z-[9999] flex flex-row justify-between items-center bg-main px-5 py-3">
-			<div>
-				<img className="h-10" src={imageLogo}></img>
-			</div>
-			<div className="flex flex-row gap-10 items-center">
-				<div className="flex flex-row gap-3 bg-[#0B2136] items-center rounded-full px-4 py-1">
-					<img className="h-5" src={imagefpt}></img>
-					<div className="caption1 text-inputHiddenColor">uniGate</div>
-				</div>
-				<div className="flex flex-row gap-2 items-center">
-					<img className="h-10 aspect-square rounded-full" src={avatar} alt="User"></img>
-					<div className="flex flex-col gap-1">
-						<div className="subtitle1 font-semibold">{username}</div>
-						<LogoutButton/>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
+    const username = useSelector((state) => state.auth?.username || '');
+    const avatar = useSelector((state) => state.auth?.avatar || imageUserLogo);
+    return (
+        <header className="sticky text-primary border-b-2 top-0 z-[9999] flex flex-row justify-between items-center bg-main px-5 py-3">
+            <div>
+                <img className="h-10" src={imageLogo}></img>
+            </div>
+            <div className="flex flex-row gap-10 items-center">
+                <div className="flex flex-row gap-3 bg-[#0B2136] items-center rounded-full px-4 py-1">
+                    <img className="h-5" src={imagefpt}></img>
+                    <div className="caption1 text-inputHiddenColor">
+                        uniGate
+                    </div>
+                </div>
+                <div className="flex flex-row gap-2 items-center">
+                    <img
+                        className="h-10 aspect-square rounded-full"
+                        src={avatar}
+                        alt="User"
+                    ></img>
+                    <div className="flex flex-col gap-1">
+                        <div className="subtitle1 font-semibold">
+                            {username}
+                        </div>
+                        <LogoutButton />
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
--- front-end/src/layout/HomeLayout/HomeLayout.jsx
+++ front-end/src/layout/HomeLayout/HomeLayout.jsx
@@ -4,20 +4,24 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-
+import './HomeLayout.scss';
 function HomeLayout() {
     const [collapsed, setCollapsed] = useState(false);
 
     return (
         <>
-            <Header />
-            <div className="flex flex-row">
-                <NavigateMenu />
-                <div className="grow w-auto px-5">
-                    <Outlet />
+            <div className="layout-default">
+                <Header />
+                <div className="flex flex-row main-content">
+                    <div className="sidebar">
+                        <NavigateMenu />
+                    </div>
+                    <div className="grow w-auto px-5">
+                        <Outlet />
+                    </div>
                 </div>
+                <Footer />
             </div>
-            <Footer />
         </>
     );
 }
