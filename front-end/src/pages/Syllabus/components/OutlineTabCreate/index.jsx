637c9261d4aba838f126bc3923edb04259f74a6d
nhutnguyen <nguyenquocnhut11c1@gmail.com>
2024-08-07 23:23:45 +0700

    fix pie chart syllabus

--- front-end/src/pages/Syllabus/components/OutlineTabCreate/index.jsx
+++ front-end/src/pages/Syllabus/components/OutlineTabCreate/index.jsx
@@ -13,10 +13,16 @@ OutlineTabCreate.PropTypes = {
     deliveryTypeList: PropTypes.object,
 };
 
-function OutlineTabCreate({ dataSource, outputStandardList, deliveryTypeList }) {
+function OutlineTabCreate({
+    dataSource,
+    outputStandardList,
+    deliveryTypeList,
+}) {
     const dispatch = useDispatch();
 
-    const syllabusDays = useSelector((state) => state.updateSyllabus.syllabusDay);
+    const syllabusDays = useSelector(
+        (state) => state.updateSyllabus.syllabusDay
+    );
 
     const flattenUnitChapters = syllabusDays.reduce((acc, cur) => {
         const chapters = cur.syllabusUnits.reduce((acc1, cur1) => {
@@ -29,42 +35,66 @@ function OutlineTabCreate({ dataSource, outputStandardList, deliveryTypeList }) 
         return [...acc, ...chapters];
     }, []);
 
-    const totalDuration = flattenUnitChapters.reduce((acc, cur) => acc + cur.duration, 0);
+    const totalDuration = flattenUnitChapters.reduce(
+        (acc, cur) => acc + cur.duration,
+        0
+    );
 
-    const deliveryType = flattenUnitChapters.reduce((acc, cur) => [...acc, cur.deliveryTypeId], []);
+    const deliveryType = flattenUnitChapters.reduce(
+        (acc, cur) => [...acc, cur.deliveryTypeId],
+        []
+    );
 
-    const totalDurationPerDeliveryType = deliveryType.map((type) => {
+    const filterDeliveryType = [];
+
+    deliveryType.forEach((item) => {
+        if (!filterDeliveryType.includes(item)) {
+            filterDeliveryType.push(item);
+        }
+
+    });
+
+    const totalDurationPerDeliveryType = filterDeliveryType.map((type) => {
         return {
             name: deliveryTypeList.find((typ) => typ.id === type)?.name || null,
             duration: flattenUnitChapters.reduce((acc, cur) => {
-                return cur.deliveryTypeId === type ? acc + cur.duration : acc + 0;
+                return cur.deliveryTypeId === type
+                    ? acc + cur.duration
+                    : acc + 0;
             }, 0),
         };
     });
 
-    const percentDurationPerDeliveryType = totalDurationPerDeliveryType.map((type) => {
-        return {
-            ...type,
-            value: ((type.duration / totalDuration) * 100).toFixed(0),
-        };
-    });
+    const percentDurationPerDeliveryType = totalDurationPerDeliveryType.map(
+        (type) => {
+            return {
+                ...type,
+                value: ((type.duration / totalDuration) * 100).toFixed(0),
+            };
+        }
+    );
 
-    const headLegendList = percentDurationPerDeliveryType.slice(0, percentDurationPerDeliveryType.length - 1);
+    const headLegendList = percentDurationPerDeliveryType.slice(
+        0,
+        percentDurationPerDeliveryType.length - 1
+    );
 
-    const headTotalPercent = headLegendList.reduce((acc, cur) => acc + Number(cur.value), 0);
+    const headTotalPercent = headLegendList.reduce(
+        (acc, cur) => acc + Number(cur.value),
+        0
+    );
 
     if (percentDurationPerDeliveryType.length >= 1) {
-        percentDurationPerDeliveryType[percentDurationPerDeliveryType.length - 1].value = 100 - headTotalPercent;
+        percentDurationPerDeliveryType[
+            percentDurationPerDeliveryType.length - 1
+        ].value = 100 - headTotalPercent;
     }
 
     return (
         <div className={styles.container + ' outline-tab-create'}>
             <div className={styles.body}>
                 {syllabusDays.map((day, idx) => (
-                    <div
-                        className={styles['day-container']}
-                        key={idx}
-                    >
+                    <div className={styles['day-container']} key={idx}>
                         <SyllabusDayCreate
                             dayInfo={day}
                             indexDay={idx}
@@ -76,7 +106,11 @@ function OutlineTabCreate({ dataSource, outputStandardList, deliveryTypeList }) 
                 ))}
                 <div className={styles['add-day-btn']}>
                     <Button
-                        style={{ backgroundColor: '#2D3748', color: '#fff', fontWeight: 700 }}
+                        style={{
+                            backgroundColor: '#2D3748',
+                            color: '#fff',
+                            fontWeight: 700,
+                        }}
                         icon=<IoAddCircleOutline size={20} />
                         onClick={() => dispatch(addDay())}
                     >
